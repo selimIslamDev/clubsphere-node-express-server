d7da3982f3753014a11afd61a5d55b4db760558a
Md salim islam <salimislam0036@gmail.com>
2026-03-21 15:28:25 +0600

    fix: replace serviceKey.json with environment variables for Firebase Admin SDK

--- config/firebase.js
+++ config/firebase.js
@@ -1,29 +1,10 @@
-// import admin from "firebase-admin";
-
-// const serviceAccount = {
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-// };
-
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//   });
-// }
-
-// export default admin;
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const serviceAccount = JSON.parse(
-  readFileSync(join(__dirname, "serviceKey.json"), "utf8")
-);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
